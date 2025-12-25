cca2f8c9614bdd929eff6436f470bebe3b8991b4
artempapkov <doctorganni@gmail.com>
2025-12-25 21:26:33 +0700

    Дз Python №5 файлы домашек

---
+++ manager.py
@@ -0,0 +1,71 @@
+import sys
+import os
+import shutil
+
+import menu
+
+while True:
+    menu.show_menu()
+
+    i = 0
+    items = os.listdir()
+    current_dir = os.getcwd()
+
+    choice = input('Выберите пункт меню')
+    if choice == '1':
+        if not os.path.exist(f'new{i}'):
+            os.mkdir(f'new{i}')
+            i += 1
+        pass
+    elif choice == '2':
+        os.rmdir(f'new{i}')
+        pass
+    elif choice == '3':
+        file_to_copy = input('Введите название папки или файла: ')
+        copied_file = input('Введите куда копировать: ')
+        if not os.path.exist(file_to_copy):
+            print('Такого файла или папки нет')
+        else:
+            shutil.copy(file_to_copy, copied_file)
+        pass
+    elif choice == '4':
+        print(os.listdir())
+        pass
+    elif choice == '5':
+        for item in items:
+            if os.path.isfile(item):
+                print('Вот только файлы: ', item)
+        pass
+    elif choice == '6':
+        for item in items:
+            if os.path.isdir(item):
+                print('Вот только папки: ', item)
+        pass
+    elif choice == '7':
+        print(sys.platform)
+        pass
+    elif choice == '8':
+        user = os.environ.get('USERNAME') or os.environ.get('USER')
+        print('Создатель программы:', user)
+        pass
+    elif choice == '9':
+        menu.victorina()
+        pass
+    elif choice == '10':
+        menu.my_bank_account()
+        pass
+    elif choice == '11':
+        old_dir = current_dir
+        new_dir = input('Введите новую директорию: ')
+        if os.path.isdir(new_dir):
+            os.chdir(new_dir)
+        else:
+            print('Такой директории не существует')
+        pass
+    elif choice == '12':
+        os.chdir(old_dir)
+        pass
+    elif choice == '0':
+        break
+    else:
+        print('Неверный пункт меню')
---
+++ menu.py
@@ -0,0 +1,134 @@
+import random
+import sys
+
+"""
+Функция по меню
+"""
+def show_menu():
+    print('1. создать папку')
+    print('2. удалить (файл/папку)')
+    print('3. копировать (файл/папку)')
+    print('4. просмотр содержимого рабочей директории')
+    print('5. посмотреть только папки')
+    print('6. посмотреть только файлы')
+    print('7. просмотр информации об операционной системе')
+    print('8. создатель программы')
+    print('9. играть в викторину')
+    print('10. мой банковский счет')
+    print('11. смена рабочей директории')
+    print('12. вернуться в прошлую директорию')
+    print('0. выход')
+    return
+
+"""
+Функция по викторине
+"""
+def victorina():
+
+    right_data = {
+        'saharovBirthDate': '01.01.1921',
+        'gagarinBirthDate': '01.01.1934',
+        'korolevBirthDate': '01.01.1907',
+        'tsiolkovskiyBirthDate': '01.01.1857',
+        'pavlovBirthDate': '02.01.1849',
+        'paoloBirthDate': '03.02.1931',
+        'leoBirthDate': '04.03.1989',
+        'jackBirthDate': '05.04.1954',
+        'martinBirthDate': '05.02.1956',
+        'petrBirthDate': '07.04.1939'
+    }
+
+    while True:
+
+        print('Начинаем викторииину по знаменитным людям!')
+
+        asked_persons = ['Андрей Сахаров', 'Юрий Гагарин', 'Сергей Королев', 'Константин Циолковский', 'Иван Павлов', 'Paolo', 'Leo', 'Jack', 'Martin', 'Petr']
+        answers = []
+        try:
+            for randomPerson in range(5):
+                randomPerson = random.sample(asked_persons, 1)
+                response = input(f'Когда родился {randomPerson}? ')
+                answers.append(response)
+        except ValueError:
+            print('Верный формат — dd.mm.yyyy. Начните викторину заново.')
+            break
+
+        correct_answers = list(right_data.values())
+        #Проверяем правильные ответы: сравнимваем список из значений словаря right_data и значения списка answers. Считаем, сколько правильных. Вычитаем их из общего кол-ва ответов. Считаем процент.
+        matches = 0
+        for answer in answers:
+            if answer in correct_answers:
+                matches += 1
+        if matches == 5:
+            print('Все правильно! Вау! 100% правильности! Ошибок нет!')
+        else:
+            print('Есть ошибки, \n ошибок: ', len(answers) - matches, '\n правильно: ', matches, '\n доля правильных ответов: ', (matches / len(answers)) * 100, '%')
+
+        #спрашиваем хочет ли юзер еще раз пройти
+        one_more_test = input('Хотите пройти тест еще раз?\n Yes/No \n')
+        if one_more_test != 'Yes':
+            continue
+        elif one_more_test != 'No':
+            break
+        else:
+            print('Я тебя не понял, поэтому заканчиваю')
+            break
+        print('Викторина окончена!')
+    pass
+
+"""
+Функция по банковскому счету
+"""
+def my_bank_account():
+    def top_up(balance, history):
+        count = int(input('Введите сумму пополнения: '))
+        balance += count
+        history.append(f'Пополнение на: {count}')
+        return balance
+
+    def buy_smth(balance, history):
+        buy_amount = int(input('Введите сумму покупки: '))
+        if buy_amount > balance:
+            print('Сумма покупки превышает баланс. Увы, купить не выйдет, пополните баланс.')
+            return balance
+        else:
+            name_item = input('Введите название покупки: ')
+            balance -= buy_amount
+            history.append(f'Покупка на {buy_amount}, для {name_item}')
+            return balance
+
+    def account_history(history):
+        for item in history:
+            print(item)
+
+    def simple_separator():
+        separator = print('**********')
+        return separator
+        pass
+
+    balance = 0
+    history = []
+
+    while True:
+        print(f'Текущий баланс: {balance}')
+        print('1. пополнение счета')
+        print('2. покупка')
+        print('3. история покупок')
+        print('4. выход')
+        simple_separator()
+        choice = input('Выберите пункт меню: ')
+        if choice == '1':
+            balance = top_up(balance, history)
+            pass
+        elif choice == '2':
+            balance = buy_smth(balance, history)
+            pass
+        elif choice == '3':
+            account_history(history)
+            pass
+        elif choice == '4':
+            sys.exit()
+            break
+        else:
+            print('Неверный пункт меню')
+    return
